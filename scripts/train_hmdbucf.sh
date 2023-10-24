a94b7d7607ca7f65d2fcffd679664fd81a74a41f
KunyuLin <kunyulin14@outlook.com>
2023-10-24 13:27:35 +0800

    Add files via upload

--- scripts/train_hmdbucf.sh
+++ scripts/train_hmdbucf.sh
@@ -1,7 +1,7 @@
 # bash
 for ((i=0;i<=2;i++))
 do
-CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py ucfhmdb RGB \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py hmdbucf RGB \
      --arch resnet50 --num_segments 5 \
      --num-spatial-group 4 --entropy-min 0.15 --entropy-mean-max 0.15 --cross-relation 0.5 \
      --mix-layers 1 2 3 --mix-alpha 0.1 --mix-prob 0.5 \
